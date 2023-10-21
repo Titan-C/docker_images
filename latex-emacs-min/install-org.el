5f7ffb2706f058e39e1fbc3ca2dbcb6b9b73a04b
Oscar Najera <hi@oscarnajera.com>
2023-10-21 22:55:17 +0200

    orgmode from GNU ELPA

--- latex-emacs-min/install-org.el
+++ latex-emacs-min/install-org.el
@@ -1,18 +1,16 @@
 (require 'package)
-(setq package-archives '(
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
-   (package-refresh-contents)
-   (package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
-    (require 'use-package)
-    (setq use-package-verbose t
-          use-package-always-ensure t))
+  (require 'use-package)
+  (setq use-package-verbose t
+        use-package-always-ensure t))
 
 (use-package org
   :ensure org-plus-contrib
